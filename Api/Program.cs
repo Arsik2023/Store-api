d305f922db1f4811f7c50d5743b4452c44e96845
arsik <arsiktimic@gmail.com>
2025-01-02 13:51:14 +0500

    Связывание с бд. Миграции

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,16 @@
+using Api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection"));
+}
+);
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
