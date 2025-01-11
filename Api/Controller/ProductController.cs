78552f5258aeb5d8ef749745c9380b8ab9fa3c1a
arsik <arsiktimic@gmail.com>
2025-01-12 02:51:03 +0500

    Новый endpoint получения продукта по id

--- Api/Controller/ProductController.cs
+++ Api/Controller/ProductController.cs
@@ -24,5 +24,39 @@ namespace MyApp.Namespace
             };
             return Ok(responseServer);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    IsSucces = false,
+                    ErrorMessage = { "Неверный id" }
+                });
+            }
+            var product = await dbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    IsSucces = false,
+                    ErrorMessage = { "Продукт по указанному id не найден" }
+                }
+                );
+            }
+            else
+            {
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Result = product
+                });
+            }
+
+        }
     }
 }
