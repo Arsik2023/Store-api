11271886deddddb9de80762d6b4cd7372634ffcc
arsik <arsiktimic@gmail.com>
2025-01-24 22:58:17 +0500

    Ещё плюсом 3 эндпоинта

--- Api/Controller/OrderController.cs
+++ Api/Controller/OrderController.cs
@@ -1,10 +1,9 @@
+using System.Net;
 using Api;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyApp.Namespace
 {
-    [Route("api/[controller]")]
-    [ApiController]
     public class OrderController : StoreController
     {
         private readonly OrdersService ordersService;
@@ -14,5 +13,149 @@ namespace MyApp.Namespace
         {
             this.ordersService = ordersService;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseServer>> CreateOrder(
+            [FromBody] OrderHeaderCreateDto orderHeaderCreateDto
+        )
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSucces = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Неверное состояние модели заказа" }
+                });
+            }
+
+            try
+            {
+                var order = await ordersService.CreateOrderAsync(orderHeaderCreateDto);
+
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.Created,
+                    Result = order
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSucces = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Невероятная ошибка", ex.Message }
+                });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResponseServer>> GetOrder(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSucces = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Неверный идентификатор заказа" }
+                });
+            }
+
+            try
+            {
+                var orderHeader = await ordersService.GetOrderByIdAsync(id);
+
+                if (orderHeader == null)
+                {
+                    return NotFound(new ResponseServer
+                    {
+                        IsSucces = false,
+                        StatusCode = HttpStatusCode.NotFound,
+                        ErrorMessages = { "Заказ не найден" }
+                    });
+                }
+
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Result = orderHeader
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseServer
+                {
+                    IsSucces = false,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    ErrorMessages = { "Что-то пошло не так", ex.Message }
+                });
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseServer>> GetOrdersByUserId(string userId)
+        {
+            try
+            {
+                var orderHeaders = await ordersService.GetOrderByUserIdAsync(userId);
+
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Result = orderHeaders
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError,
+                new ResponseServer
+                {
+                    IsSucces = false,
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    ErrorMessages = { "При обработке возникла проблема", ex.Message }
+                });
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ResponseServer>> UpdateOrderHeader(
+            int id, [FromBody] OrderHeaderUpdateDto orderHeaderUpdateDto
+        )
+        {
+            try
+            {
+                var success = await ordersService
+                    .UpdateOrderHeaderAsync(id, orderHeaderUpdateDto);
+
+                if (!success)
+                {
+                    return BadRequest(new ResponseServer
+                    {
+                        IsSucces = false,
+                        StatusCode = HttpStatusCode.BadRequest,
+                        ErrorMessages = { "Обновление пошло не по плану" }
+                    });
+                }
+
+                return Ok(new ResponseServer
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Result = new { Results = true, Text = "Всё обновлено" }
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError,
+                    new ResponseServer
+                    {
+                        IsSucces = false,
+                        StatusCode = HttpStatusCode.InternalServerError,
+                        ErrorMessages = { "При обновлении проблема", ex.Message }
+                    });
+            }
+        }
     }
 }
+
+
--- Api/Service/OrdersService.cs
+++ Api/Service/OrdersService.cs
@@ -1,4 +1,6 @@
-﻿namespace Api;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api;
 
 public class OrdersService
 {
@@ -20,6 +22,7 @@ public class OrdersService
             CustomerEmail = orderHeaderCreateDto.CustomerEmail,
             OrderTotalAmount = orderHeaderCreateDto.OrderTotalAmount,
             TotalCount = orderHeaderCreateDto.TotalCount,
+            OrderDateTime = DateTime.UtcNow,
             Status = string.IsNullOrEmpty(orderHeaderCreateDto.Status)
                 ? SharedData.OrderStatus.Pending
                 : orderHeaderCreateDto.Status
@@ -42,5 +45,67 @@ public class OrdersService
         }
         await dbContext.SaveChangesAsync();
         return order;
+    }
+
+    public async Task<OrderHeader> GetOrderByIdAsync(int id)
+    {
+        return await dbContext
+        .OrderHeaders
+        .Include(items => items.OrderDetailItems)
+        .ThenInclude(x => x.Product)
+        .FirstOrDefaultAsync(u => u.OrderHeaderId == id);
+    }
+
+    public async Task<IEnumerable<OrderHeader>> GetOrderByUserIdAsync(string userId)
+    {
+        var query = dbContext
+            .OrderHeaders
+            .Include(items => items.OrderDetailItems)
+            .OrderByDescending(u => u.AppUserId);
+
+        if (!string.IsNullOrEmpty(userId))
+        {
+            return await query
+                .Where(u => u.AppUserId == userId)
+                .ToListAsync();
+        }
+        return await query.ToListAsync();
+    }
+
+    public async Task<bool> UpdateOrderHeaderAsync(int id, OrderHeaderUpdateDto orderHeaderUpdateDto)
+    {
+        if (orderHeaderUpdateDto == null
+            || orderHeaderUpdateDto.OrderHeaderId != id)
+        {
+            return false;
+        }
+
+        var orderHeaderFromDb = await dbContext
+            .OrderHeaders
+            .FirstOrDefaultAsync(u => u.OrderHeaderId == id);
+
+        if (orderHeaderFromDb == null)
+        {
+            return false;
+        }
+
+        if (!string.IsNullOrEmpty(orderHeaderUpdateDto.CustomerName))
+        {
+            orderHeaderFromDb.CustomerName = orderHeaderUpdateDto.CustomerName;
+        }
+
+        if (!string.IsNullOrEmpty(orderHeaderUpdateDto.CustomerEmail))
+        {
+            orderHeaderFromDb.CustomerEmail = orderHeaderUpdateDto.CustomerEmail;
+        }
+
+        if (!string.IsNullOrEmpty(orderHeaderUpdateDto.Status))
+        {
+            orderHeaderFromDb.Status = orderHeaderUpdateDto.Status;
+        }
+
+        await dbContext.SaveChangesAsync();
+
+        return true;
     }
 }
